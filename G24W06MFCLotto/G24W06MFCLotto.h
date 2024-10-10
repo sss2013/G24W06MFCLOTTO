1c8dd93bb0f7968bad0bcffa9fe7135a4185b99d
sss2013 <kshi9478@gmail.com>
2024-10-10 15:47:12 +0900

    초기 커밋.

---
+++ G24W06MFCLotto/G24W06MFCLotto.h
@@ -0,0 +1,32 @@
+﻿
+// G24W06MFCLotto.h: PROJECT_NAME 애플리케이션에 대한 주 헤더 파일입니다.
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH에 대해 이 파일을 포함하기 전에 'pch.h'를 포함합니다."
+#endif
+
+#include "resource.h"		// 주 기호입니다.
+
+
+// CG24W06MFCLottoApp:
+// 이 클래스의 구현에 대해서는 G24W06MFCLotto.cpp을(를) 참조하세요.
+//
+
+class CG24W06MFCLottoApp : public CWinApp
+{
+public:
+	CG24W06MFCLottoApp();
+
+// 재정의입니다.
+public:
+	virtual BOOL InitInstance();
+
+// 구현입니다.
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CG24W06MFCLottoApp theApp;
---
+++ G24W06MFCLotto/G24W06MFCLottoDlg.h
@@ -0,0 +1,43 @@
+﻿
+// G24W06MFCLottoDlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CG24W06MFCLottoDlg 대화 상자
+class CG24W06MFCLottoDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CG24W06MFCLottoDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_G24W06MFCLOTTO_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBtnGenerateClicked();
+protected: 
+	CString Num1;
+	CString Num2;
+	CString Num3;
+	CString Num4;
+	CString Num5;
+	CString Num6;
+};
